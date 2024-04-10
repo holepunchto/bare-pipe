7068541f1f0ae08496904ccdfaffdcf6cdc45f88
Kasper Isager Dalsgarð <kasperisager@hey.com>
2024-04-10 13:46:12 +0200

    Namespace `init()` method

--- binding.c
+++ binding.c
@@ -699,7 +699,7 @@ bare_pipe_unref (js_env_t *env, js_callback_info_t *info) {
 }
 
 static js_value_t *
-init (js_env_t *env, js_value_t *exports) {
+bare_pipe_exports (js_env_t *env, js_value_t *exports) {
   int err;
 
 #define V(name, fn) \
@@ -728,4 +728,4 @@ init (js_env_t *env, js_value_t *exports) {
   return exports;
 }
 
-BARE_MODULE(bare_pipe, init)
+BARE_MODULE(bare_pipe, bare_pipe_exports)
