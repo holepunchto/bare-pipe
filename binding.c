cd415bdd16b65f3a08b22c347812e4604d9ea233
Kasper Isager Dalsgarð <kasperisager@hey.com>
2024-11-28 10:52:41 +0100

    Avoid UAF when socket unsets its handle

--- binding.c
+++ binding.c
@@ -274,8 +274,6 @@ bare_pipe__on_close(uv_handle_t *handle) {
 
   js_env_t *env = pipe->env;
 
-  if (pipe->exiting) goto finalize;
-
   js_handle_scope_t *scope;
   err = js_open_handle_scope(env, &scope);
   assert(err == 0);
@@ -288,12 +286,6 @@ bare_pipe__on_close(uv_handle_t *handle) {
   err = js_get_reference_value(env, pipe->on_close, &on_close);
   assert(err == 0);
 
-  js_call_function(env, ctx, on_close, 0, NULL, NULL);
-
-  err = js_close_handle_scope(env, scope);
-  assert(err == 0);
-
-finalize:
   err = js_finish_deferred_teardown_callback(pipe->teardown);
   assert(err == 0);
 
@@ -316,6 +308,11 @@ finalize:
   assert(err == 0);
 
   err = js_delete_reference(env, pipe->ctx);
+  assert(err == 0);
+
+  if (!pipe->exiting) js_call_function(env, ctx, on_close, 0, NULL, NULL);
+
+  err = js_close_handle_scope(env, scope);
   assert(err == 0);
 }
 
